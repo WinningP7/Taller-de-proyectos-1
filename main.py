0a38956ae768e6e5baaf48990f22c5ecd0eeb59f
DTom <t0mm3ndx@protonmail.com>
2020-09-23 17:20:37 -0500

    Pantalla prueba
    pantalla vacia

---
+++ main.py
@@ -0,0 +1,17 @@
+import kivy 
+kivy.require('1.9.0')
+
+from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout 
+
+class Contenedor_01(BoxLayout):
+    None
+
+
+class MainApp(App):
+    title = "Hola mundo"
+    def build(self):
+        return Contenedor_01()
+
+if __name__ == "__main__":
+    MainApp().run()
