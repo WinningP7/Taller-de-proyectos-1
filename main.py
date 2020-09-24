8e2b9913ad0ed71ba554fb740ee8c0ee5518b5dd
Dtomdx <56743170+Dtomdx@users.noreply.github.com>
2020-09-24 15:09:10 -0500

    Dimensiones android
    Dimensiones configuradas

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import kivy 
-kivy.require('1.9.0')
-
 from kivy.app import App 
-from kivy.uix.boxlayout import BoxLayout 
+from  kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class Contenedor_01(BoxLayout):
-    None
+#configurar pantalla
+from kivy.config import Config 
+#ancho
+Config.set("graphics", "width","340")
+#largo
+Config.set("graphics", "height","640")
 
+class MainWid(ScreenManager):
+    #definiendo metodo init con parametros en diccionario
+    def __init__(self, **kwarg):
+        super(MainWid, self).__init__()
+        self.StartWid = StartWid()
+        wid = Screen(name = "start")
+        wid.add_widget(self.StartWid)
+        self.add_widget(wid)
+
+class StartWid(BoxLayout):
+    pass
 
 class MainApp(App):
-    title = "Hola mundo"
+    title = "TIME BUS"
     def build(self):
-        return Contenedor_01()
+        return MainWid()
+
+
 
 if __name__ == "__main__":
     MainApp().run()
