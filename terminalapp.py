570cddb5c3438a264854e12864ab8d0c4f41e1e7
DTom <t0mm3ndx@protonmail.com>
2020-09-29 23:02:44 -0500

    TimeBus
    Pantalla de visualicion de origen y terminal ..
    Falta añadir funcionalidad
    Falta raspado wewb

--- terminalapp.py
+++ terminalapp.py
@@ -3,12 +3,16 @@ from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
+
 #configurar pantalla
 from kivy.config import Config 
 #ancho
 Config.set("graphics", "width","340")
 #largo
 Config.set("graphics", "height","640")
+
+from kivy.uix.dropdown import DropDown 
+
 
 
 class MainWindow(Screen):
